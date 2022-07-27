c846df9034de4cbddd6026f15a5c90c4a21a3b24
Mike Bild <mike@mikebild.com>
2022-07-27 13:46:56 +0200

    upgrade NPM deps

--- deploy.js
+++ deploy.js
@@ -3,13 +3,13 @@ import { AWSAdapterStack } from 'sveltekit-adapter-aws';
 import { IntrastructureStack } from './infrastructure.js';
 
 const app = new App();
-app.region = 'us-east-1';
+app.region = process.env.CDK_DEFAULT_REGION;
 app.account = process.env.CDK_DEFAULT_ACCOUNT;
 
-const { serverHandler } = new AWSAdapterStack(app, 'Sveltekit-Adapter-AWS-App-Stack', {
-  FQDN: 'sveltekit-adapter-aws-advanced-demo.mikebild.com',
+const { serverHandler } = new AWSAdapterStack(app, 'sveltekit-adapter-aws-advanced-app', {
+	FQDN: 'sveltekit-adapter-aws-advanced-demo.mikebild.com'
 });
 
-new IntrastructureStack(app, 'Sveltekit-Adapter-AWS-Intrastructure-Stack', {
-  serverHandler,
+new IntrastructureStack(app, 'sveltekit-adapter-aws-advanced-infrastructure', {
+	serverHandler
 });
