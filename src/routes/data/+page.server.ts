c75b4f2c27f10ec1e92cbac7a96d069d7441345f
Mike Bild <mike@mikebild.com>
2022-08-23 08:51:21 +0200

    update infrastructure config

--- src/routes/data/+page.server.ts
+++ src/routes/data/+page.server.ts
@@ -1,8 +1,9 @@
 import AWS from 'aws-sdk';
+import { TABLENAME, REGION } from '$env/static/private';
 
 export async function load() {
-	const ddb = new AWS.DynamoDB();
-	const result = await ddb.scan({ TableName: process.env.TABLENAME! }).promise();
+	const ddb = new AWS.DynamoDB({ region: REGION });
+	const result = await ddb.scan({ TableName: TABLENAME! }).promise();
 
 	return {
 		items: result.Items?.map(({ id, type }: any) => ({ id: id.S, type: type.S })).sort(
